cbbaa279467108fe79ccc8a0163de8de1123240b
PE Lieb <lieb.pe@gmail.com>
2017-05-30 20:54:09 +0200

    [UPD] index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,10 @@ export default async function handleAction(res, payload) {
   const currentAction = res.action && res.action.slug
   console.log(currentAction)
   let replies = []
-  if (res.reply()) {
+  if (actions[currentAction]) {
+    console.log('Enter action')
+    replies = await actions[currentAction].default(res, payload)
+  } else if (res.reply()) {
     replies.push({
       type: 'text',
       content: res.reply(),
