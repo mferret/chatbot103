858f6ae8d611e6e1ecef28fa2df73ffc6ddb8e41
maxime.ferret <maxime.ferret@MacBook-Pro-de-admin.local>
2017-06-30 16:17:53 +0200

    show website test 2

--- src/actions/show-website.js
+++ src/actions/show-website.js
@@ -1,13 +1,13 @@
 const agent = require('superagent-promise')(require('superagent'), Promise)
 const formatter = require('../formatter')
-const languages = ['Facebook', 'Twitter', 'instagram', 'LinkedIn',]
+const WEBSITE = ['Facebook', 'Twitter', 'instagram', 'LinkedIn',]
 
 export default async function showWebsite(res) {
   console.log('SHOW WEBSITE')
 
   const replies = []
   const quickReplies = []
-  languages.forEach((l) => {
+  WEBSITE.forEach((l) => {
     quickReplies.push({
       name: l,
       value: `Top starred repos in ${l}`,
